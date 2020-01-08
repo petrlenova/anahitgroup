464da2f566162c277b47a050843c4936b24a8f82
petrlenova <petr_lenova@mail.ru>
2020-01-08 20:33:43 +0400

    8

---
+++ js/global.js
@@ -0,0 +1,14 @@
+<script>
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("webs-nav");
+var sticky = navbar.offsetTop;
+document.getElementById("webs-nav").style.zIndex = "10000";
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+</script>
