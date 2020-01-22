110b2c3139308525328afe1ff1f9502b77a1a708
petrlenova <petr_lenova@mail.ru>
2020-01-23 02:58:06 +0400

    27

--- fpm/alltours/day_data.js
+++ fpm/alltours/day_data.js
@@ -17,14 +17,14 @@ var alltours = {
             title: 'Excursion to  Tsaghkadzor – Sevan. ',
             description: 'Breakfast in the hotel.Excursion to  Tsaghkadzor.            Tsaghkadzor is a city in the Kotayk region of Armenia, a popular ski and climate resort; located 50 km northeast of Yerevan and 5 km from the regional center, Hrazdan. Tsaghkadzor is a popular place for both winter and summer tourism. In the city and its surroundings there are a number of hotels of different classes of service, from 2 * to 5*.            Excursion to Sevan.             Lake Sevan is the largest body of water in Armenia and the Caucasus region, and one of the largest freshwater high-altitude (alpine) lakes in Eurasia. The lake is situated in Gegharkunik Province, at an altitude of 1,900 m (6,234 ft) above sea level. The total surface area of its basin is about 5,000 km2 (1,900 sq mi), which makes up  1⁄6 of Armenias territory. The lake itself is 1,242 km2 (480 sq mi), and the volume is 32.8 km3 (7.9 cu mi). It is fed by 28 rivers and streams. Only 10% of the incoming water is drained by the Hrazdan River, while the remaining 90% evaporates.  Return to the city of Yerevan.    Overnight at the hotel.',
             src1: 'https://raw.githubusercontent.com/petrlenova/anahitgroup/master/fpm/img/summer/YerevanTsaghkadzorSevanGarniGeghardYerevan/3.jpg',
-            src2: 'https://raw.githubusercontent.com/petrlenova/anahitgroup/master/fpm/img/summer/YerevanTsaghkadzorSevanGarniGeghardYerevan/4.jpg'
+            src2: 'https://raw.githubusercontent.com/petrlenova/anahitgroup/master/fpm/img/summer/YerevanTsaghkadzorSevanGarniGeghardYerevan/2.jpg'
            },
            
           3:{
             title: 'Breakfast in the hotel. ',
             description: 'Breakfast in the hotel.            A sightseeing tour in Yerevan, which includes visiting such attractions as the Mother Armenia, Tsitsernakaberd .            Visiting in Dallma mall.            Overnight at the hotel            ',
-            src1: 'https://raw.githubusercontent.com/petrlenova/anahitgroup/master/fpm/img/summer/YerevanTsaghkadzorSevanGarniGeghardYerevan/3.jpg',
-            src2: 'https://raw.githubusercontent.com/petrlenova/anahitgroup/master/fpm/img/summer/YerevanTsaghkadzorSevanGarniGeghardYerevan/4.jpg'
+            src1: 'https://raw.githubusercontent.com/petrlenova/anahitgroup/master/fpm/img/summer/YerevanTsaghkadzorSevanGarniGeghardYerevan/4.jpg',
+            src2: 'https://raw.githubusercontent.com/petrlenova/anahitgroup/master/fpm/img/summer/YerevanTsaghkadzorSevanGarniGeghardYerevan/5.jpg'
            },
            
           4:{
@@ -37,7 +37,7 @@ var alltours = {
           5:{
             title: 'Free day ',
             description: ' ',
-            src1: '',
+            src1: 'https://raw.githubusercontent.com/petrlenova/anahitgroup/master/fpm/img/summer/YerevanTsaghkadzorSevanGarniGeghardYerevan/6.jpg',
             src2: ''
            },
            
@@ -45,13 +45,13 @@ var alltours = {
             title: 'Excursion to  City Yerevan- Zoo park, Water World ',
             description: 'Breakfast in the hotel.            Excursion to Zoo park- The Yerevan Zoo, also known as the Zoological Garden of Yerevan is a 35-hectare  zoo established in 1940 in Yerevan, Armenia. At present the zoo is home to about 2749 individuals representing 204 species.            Visiting in Water World. Yerevan Water World was opened in 2001 in one of the most picturesque sites of Yerevan on Myasnikyan Avenue, lying between the Yerevan Zoo and the Botanical Garden, with an area of 2.5 hectares. The open-air park is home to 2 large pools, 1 children\'s pool, 1 VIP pool and 3 pools for water slide attractions. There are food and beverage outlets and a restaurant with a capacity of 120 people. During winter time, the largest pool of the park is urned into an ice skating rink, offering a surface of 500 m²            Overnight at the hotel.                         ',
             src1: '',
-            src2: ''
+            src2: 'https://raw.githubusercontent.com/petrlenova/anahitgroup/master/fpm/img/summer/YerevanTsaghkadzorSevanGarniGeghardYerevan/2.jpg'
            },
            
           7:{
             title: 'Free day ',
             description: ' ',
-            src: '',
+            src1: 'https://raw.githubusercontent.com/petrlenova/anahitgroup/master/fpm/img/summer/YerevanTsaghkadzorSevanGarniGeghardYerevan/2.jpg',
             src: ''
            },
            
