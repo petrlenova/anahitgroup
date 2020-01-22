0b6422990058e6880069b130a289fb49ebf9d8ff
petrlenova <petr_lenova@mail.ru>
2020-01-22 12:27:02 +0400

    17

---
+++ fpm/alltours/day_data.js
@@ -0,0 +1,110 @@
+var alltours = {
+
+    0: {imglink: "img/summer/YerevanTsaghkadzorSevanGarniGeghardYerevan/2.jpg",
+        price: '50$',
+        weather: 'Summer',
+        daycount: '7day 8night',
+        name: 'Armenian tour',
+        days:{
+           1:{
+            title: 'Arrival to Yerevan. ',
+            description: 'Meeting at the airport. Check-in hotel.A sightseeing tour in Yerevan, the A. Spendiarov Armenian Theater of Opera and Ballet, Swam Lake.            Overnight at the hotel.',
+            src1: 'img/summer/YerevanTsaghkadzorSevanGarniGeghardYerevan/1.jpg',
+            src2: 'img/summer/YerevanTsaghkadzorSevanGarniGeghardYerevan/2.jpg'
+           },
+           
+          2:{
+            title: 'Excursion to  Tsaghkadzor – Sevan. ',
+            description: 'Breakfast in the hotel.Excursion to  Tsaghkadzor.            Tsaghkadzor is a city in the Kotayk region of Armenia, a popular ski and climate resort; located 50 km northeast of Yerevan and 5 km from the regional center, Hrazdan. Tsaghkadzor is a popular place for both winter and summer tourism. In the city and its surroundings there are a number of hotels of different classes of service, from 2 * to 5*.            Excursion to Sevan.             Lake Sevan is the largest body of water in Armenia and the Caucasus region, and one of the largest freshwater high-altitude (alpine) lakes in Eurasia. The lake is situated in Gegharkunik Province, at an altitude of 1,900 m (6,234 ft) above sea level. The total surface area of its basin is about 5,000 km2 (1,900 sq mi), which makes up  1⁄6 of Armenias territory. The lake itself is 1,242 km2 (480 sq mi), and the volume is 32.8 km3 (7.9 cu mi). It is fed by 28 rivers and streams. Only 10% of the incoming water is drained by the Hrazdan River, while the remaining 90% evaporates.  Return to the city of Yerevan.    Overnight at the hotel.',
+            src1: 'img/summer/YerevanTsaghkadzorSevanGarniGeghardYerevan/3.jpg',
+            src2: 'img/summer/YerevanTsaghkadzorSevanGarniGeghardYerevan/4.jpg'
+           },
+           
+          3:{
+            title: 'Breakfast in the hotel. ',
+            description: 'Breakfast in the hotel.            A sightseeing tour in Yerevan, which includes visiting such attractions as the Mother Armenia, Tsitsernakaberd .            Visiting in Dallma mall.            Overnight at the hotel            ',
+            src1: 'img/summer/YerevanTsaghkadzorSevanGarniGeghardYerevan/3.jpg',
+            src2: 'img/summer/YerevanTsaghkadzorSevanGarniGeghardYerevan/4.jpg'
+           },
+           
+          4:{
+            title: 'Excursion to - Garni – Geghard ',
+            description: ' Breakfast in the hotel.            Excursion to Garni. The Temple of Garni is an Ionic temple in Garni. It is the best-known structure and symbol of pre-Christian Armenia. It is the only standing Greco-Roman colonnaded building in Armenia and the former Soviet Union.            The structure was probably built by king Tiridates I in the first century AD as a temple to the sun god Mihr. After Armenias conversion to Christianity in the early fourth century, it was converted into a royal summer house of Khosrovidukht, the sister of Tiridates III. According to some scholars it was not a temple but a tomb and thus survived the universal destruction of pagan structures. It collapsed in a 1679 earthquake. Renewed interest in the 19th century led to excavations at the site in early and mid-20th century and its eventual reconstruction between 1969 and 1975. It is one of the main tourist attractions in Armenia and the central shrine of Armenian neopaganism.            Visiting in Geghard. Geghard is a medieval monastery in the Kotayk province of Armenia, being partially carved out of the adjacent mountain, surrounded by cliffs. It is listed as a UNESCO World Heritage Site.            Return to the city of Yerevan.  A sightseeing tour in Yerevan, which includes visiting such attractions as the Cascade.  Overnight at the hotel.',
+            src1: 'img/summer/YerevanTsaghkadzorSevanGarniGeghardYerevan/5.jpg',
+            src2: 'img/summer/YerevanTsaghkadzorSevanGarniGeghardYerevan/6.jpg'
+           },
+           
+          5:{
+            title: 'Free day ',
+            description: ' ',
+            src1: '',
+            src2: ''
+           },
+           
+          6:{
+            title: 'Excursion to  City Yerevan- Zoo park, Water World ',
+            description: 'Breakfast in the hotel.            Excursion to Zoo park- The Yerevan Zoo, also known as the Zoological Garden of Yerevan is a 35-hectare  zoo established in 1940 in Yerevan, Armenia. At present the zoo is home to about 2749 individuals representing 204 species.            Visiting in Water World. Yerevan Water World was opened in 2001 in one of the most picturesque sites of Yerevan on Myasnikyan Avenue, lying between the Yerevan Zoo and the Botanical Garden, with an area of 2.5 hectares. The open-air park is home to 2 large pools, 1 children\'s pool, 1 VIP pool and 3 pools for water slide attractions. There are food and beverage outlets and a restaurant with a capacity of 120 people. During winter time, the largest pool of the park is urned into an ice skating rink, offering a surface of 500 m²            Overnight at the hotel.                         ',
+            src1: '',
+            src2: ''
+           },
+           
+          7:{
+            title: 'Free day ',
+            description: ' ',
+            src: '',
+            src: ''
+           },
+           
+       }
+    },
+
+    1: {name: 'tourname2',
+       days:{
+        1:{
+            title: 'tourname2 ',
+            description: 'desk tourname2 ',
+            src: '111'
+           },
+           
+          2:{
+            title: 'tourname2-2 ',
+            description: 'desk tourname2 -2 ',
+            src: '111'
+           },
+           
+          3:{
+            title: '1 ',
+            description: '11 ',
+            src: '111'
+           },
+           
+          4:{
+            title: '1 ',
+            description: '11 ',
+            src: '111'
+           },
+           
+          5:{
+            title: '1 ',
+            description: '11 ',
+            src: '111'
+           },
+           
+          6:{
+            title: '1 ',
+            description: '11 ',
+            src: '111'
+           },
+           
+          7:{
+            title: '1 ',
+            description: '11 ',
+            src: '111'
+           },
+           
+       }
+    }
+
+ 
+
+}
